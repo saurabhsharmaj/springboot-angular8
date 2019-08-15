a0d5a3a3f57ef4deebeaaaa0a8d41cbf28abca89
saurabhsharmaj <saurabh.find@gmail.com>
2019-08-16 00:19:27 +0530

    commit.

---
+++ src/main/java/com/sample/im/common/IMUtils.java
@@ -0,0 +1,23 @@
+package com.sample.im.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class IMUtils {
+
+	public String getHostName() {
+		return "";
+	}
+
+	public String getIpAddress() {
+		return "";
+	}
+
+	public String getOS() {
+		return "";
+	}
+
+	public String machineType() {
+		return "";
+	}
+}
